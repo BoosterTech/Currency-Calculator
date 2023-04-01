5ba815d2e92b8397e957aeda3ef75c8f066fcdda
Dariusz Podczasik <boostertech@mail.com>
2023-04-01 13:10:57 +0100

    Code in file.js encapulated into functions

--- js/file.js
+++ js/file.js
@@ -1,28 +1,41 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
 
-let rateEUR = 0.21;
-let rateUSD = 0.23;
-let rateGBP = 0.19;
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let result = amountElement.value;
-    let currency = currencyElement.value;
-
-    switch (currency) {
-        case "EUR":
-            result *= rateEUR;
-            break;
-        case "USD":
-            result *= rateUSD;
-            break;
-        case "GBP":
-            result *= rateGBP;
+        formElement.addEventListener("submit", onFormSubmit);
     }
 
-    resultElement.innerHTML = `${amountElement.value} PLN = <strong>${result.toFixed(2)}</strong> ${currencyElement.value}`;
-});
+    const calculateResult = (amount, currency) => {
+        const rateEUR = 0.21;
+        const rateUSD = 0.23;
+        const rateGBP = 0.19;
+
+        switch (currency) {
+            case "EUR":
+                return amount *= rateEUR;
+            case "USD":
+                return amount *= rateUSD;
+            case "GBP":
+                return amount *= rateGBP;
+        }
+    }
+
+    const updateResult = () => {
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+        resultElement.innerHTML = `${amount} PLN = <strong>${result.toFixed(2)}</strong> ${currency}`;
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        updateResult();
+    }
+    
+    init();
+}
